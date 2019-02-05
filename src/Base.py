d87d988069e10b21b70d3e913fe67b5c95a32f6b
Sanveg Rane <sanveg.rane@somaiya.edu>
2019-02-05 23:34:44 +0530

    Added logging and refactored

--- src/Base.py
+++ src/Base.py
@@ -2,13 +2,23 @@ from src.UrlLoader import UrlLoader
 from src.WebPageReader import WebPageReader
 from src.FileWriter import FileWriter
 
+import time
 import logging as logger
 
+# basic logging to file
 logger.basicConfig(
     filename="logs/base.log",
-    level=logger.INFO,
-    format="%(asctime)s:%(levelname)s:%(message)s"
+    level=logger.DEBUG,
+    format="%(asctime)s: %(name)-12s: %(levelname)-8s:%(message)s",
 )
+
+# set up logging to console
+console = logger.StreamHandler()
+console.setLevel(logger.DEBUG)
+formatter = logger.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logger.getLogger('').addHandler(console)
+logger = logger.getLogger(__name__)
 
 
 class Base:
@@ -28,8 +38,12 @@ class Base:
 
     def __crawl_pages(self):
         for attribute in self.__page_urls:
+            start_time = time.time()
             web_page_doc = self.page_reader.read_page(self.__page_urls[attribute])
             self.__save_page(attribute, web_page_doc)
+            end_time = time.time()
+
+            logger.info("Time to fetch: " + attribute + " - " + str(end_time - start_time) + " milli-secs")
 
     def __save_page(self, file_name, page):
         self.writer.write_to_file(file_name, page)
@@ -39,5 +53,4 @@ class Base:
     Main Method
 '''
 if __name__ == '__main__':
-    base = Base()
-    base.read_web_pages()
+    Base().read_web_pages()
